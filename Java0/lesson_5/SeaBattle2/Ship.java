2fb9ac4ff6d0550f46503b84a3ac9ff3bf0d9e26
begdv72 <45359717+begdv72@users.noreply.github.com>
2019-01-01 21:50:47 +0300

    Create Ship.java

---
+++ Java0/lesson_5/SeaBattle2/Ship.java
@@ -0,0 +1,37 @@
+public class Ship {
+    int size;
+    int position;
+
+    Ship(int size, int position) {
+        this.size = size;
+        this.position = position;
+    }
+
+    //Отображение корабля на игровом поле
+    public void show(char[] cells) {
+        for (int i = this.position; i < this.position + this.size; i++)
+            cells[i - 1] = 'X';
+    }
+
+    //Корабль подбит?
+    public boolean hitted(char[] cells) {
+        boolean result = true;
+        for (int i = this.position; i < this.position + this.size; i++) {
+            if (cells[i - 1] != '#') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    //Ячейки поля, на которые нельзя размещать другие корабли
+    public boolean isOccupied(int position, int size) {
+        return (((position + size) >= this.position - 1) && (position <= this.position + this.size + 1));
+    }
+
+    //Ячейки поля, занятые кораблем
+    public boolean isPlace(int position) {
+        return ((position >= this.position) && (position <= this.position + this.size));
+    }
+}
